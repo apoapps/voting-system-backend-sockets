1fc5e88858cbaddba1364285894b5d764d08fd1a
ALEJANDRO APODACA <alejandroapodaca@Alexs-MacBook-Pro.local>
2023-11-23 02:57:57 -0800

    added flutter app to client web

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
 import express from "express";
 import userRoutes from "./api/users.js";
 import votingSessionRoutes from "./api/voting_points.js";
+import path from "path";
 
 const app = express();
 app.use(express.json());
-// Usar los enrutadores
+
+// Sirve los archivos estáticos de la carpeta 'public'
+app.use(express.static(path.join(__dirname, "public")));
+
+// Usa los enrutadores para tus rutas de API
 app.use("/users", userRoutes);
 app.use("/voting-session", votingSessionRoutes);
+
+// Para cualquier otra solicitud que no sea de API, sirve el 'index.html' de Flutter
+// Asegúrate de que esta ruta venga después de todas las rutas de API y antes de express.static
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 export default app;
